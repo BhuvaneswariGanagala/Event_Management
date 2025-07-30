78ee409c72e9b425bc0b045c150ab85140ec6743
BhuvaneswariGanagala <n210904@rguktn.ac.in>
2025-07-30 01:36:26 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const authRouter = require('./router/auth-router.js'); // Ensure the correct file extension
-const db = require('./db/utils.js'); // Include the database connection
+const authRouter = require('./router/auth-router.js'); 
+const db = require('./db/utils.js'); 
 
 const app = express();
 app.use(express.json());
